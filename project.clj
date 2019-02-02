db5305793beedfe362d0c6e8328b2db47192dbc1
Alexander Freeman <alexander.freeman.nl@gmail.com>
2019-02-02 23:22:47 +0100

    Add CLI options for parsing html

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/java.jdbc "0.7.8"]
                  [clj-tagsoup/clj-tagsoup "0.3.0"]
-                 [org.xerial/sqlite-jdbc "3.7.2"]])
+                 [org.xerial/sqlite-jdbc "3.7.2"]
+                 [org.clojure/tools.cli "0.4.1"]])
--- src/telegram_export_parser/core.clj
+++ src/telegram_export_parser/core.clj
@@ -1,7 +1,9 @@
 (ns telegram-export-parser.core
   (:require [clojure.string :as str]
             [clojure.java.jdbc :refer :all]
-            [pl.danieljanus.tagsoup :as tagsoup]))
+            [pl.danieljanus.tagsoup :as tagsoup]
+            [clojure.tools.cli :refer [parse-opts]]))
+
 
 
 (defn create-db-specs [filename]
@@ -9,7 +11,7 @@
    :subprotocol "sqlite"
    :subname     filename})
 
-(defn create-db
+(defn create-db!
   "create db and table"
   [db]
   (try
@@ -45,49 +47,73 @@
     ""
     text))
 
-(defn parse-message [message]
+
+(defn remove-blocks [text]
+  (reduce
+    (fn [string item]
+      (if (vector? item)
+        string
+        (str string item)))
+    ""
+    text))
+
+(defn parse-message [opts message]
   (let [[_ _ & text] (navigate message [:body :text])
         [_ {datetime :title} & _] (navigate message [:body :details])]
     (if (and (some? text) (some? datetime))
-      (-> text
-          (replace-blocks-with-original-text)
-          (str/trim)
-          (str/lower-case))
+      {:message (-> text
+                    ((fn [item] (if (:filter-html opts) item (remove-blocks item))))
+                    ((fn [item] (if (:extract-link-text opts) (replace-blocks-with-original-text item) item)))
+                    (str/trim)
+                    (str/lower-case))
+       :time datetime}
       nil)))
 
-(defn get-text-of-messages [root]
+(defn get-text-of-messages [root opts]
   (let [[_ _ & messages] (navigate root [:body :page_wrap :page_body :history])]
     (->> messages
-         (map parse-message)
+         (map (partial parse-message opts))
          (filter some?))))
 
-(defn insert-into-db [db message]
-  (insert! db :chatmessages {:message message}))
+(defn insert-into-db [db {:keys [message time]}]
+  (insert! db :chatmessages {:message message :time time}))
 
-
-(defn parse-file [file db]
+(defn parse-file! [file db opts]
   (let [contents (slurp file)
         html (tagsoup/parse-string contents)]
-    (doall (map (partial insert-into-db db) (get-text-of-messages html)))))
+    (doall (map (partial insert-into-db db) (get-text-of-messages html opts)))))
 
-(defn parse-directory [directory db-specs]
+(defn parse-directory! [directory db-specs opts]
   (let [files (filter
                 #(.isFile %)
                 (file-seq (clojure.java.io/file directory)))]
-    (doall (map #(parse-file %1 db-specs) files))))
-
+    (doall (map #(parse-file! %1 db-specs opts) files))))
 
 (def usage
   (->> ["Parser for telegram HTML logs"
         ""
-        "Usage: ./parser <directory> <dbfilename>"
+        "Usage: ./parser <directory> <dbfilename> <options>"
         "E.g. ./parser logs messages.db"
+        ""
+        "Options: "
         ""]
        (str/join \newline)))
 
+(def cli-options
+  [["-u" "--filter-html" "Remove html blocks. Defaults to false."]
+   ["-e" "--extract-link-text" "Extract text from hyperlinks. Defaults to false."]])
+
+(defn run-program! [directory filename opts]
+  (let [db (create-db-specs filename)]
+    (create-db! db)
+    (parse-directory! directory db opts)))
+
 (defn -main [& args]
-  (if (= (count args) 2)
-    (let [db-specs (create-db-specs (second args))]
-      (create-db db-specs)
-      (parse-directory (first args) db-specs))
-    (print usage)))
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (if errors
+      (println (str/join " " errors))
+      (if (>= (count arguments) 2)
+        (if (> (count options) 1)
+          (println (str usage summary \newline \newline "You can only choose one option."))
+          (run-program! (first args) (second args) options))
+        (println (str usage summary))))))
